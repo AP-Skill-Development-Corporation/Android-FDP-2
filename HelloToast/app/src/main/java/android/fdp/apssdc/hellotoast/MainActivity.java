2f2c14993bcbe51fb0f23057a137111f3f580b9c
Pavan kumar reddy <pavankumart46@gmail.com>
2020-06-03 10:14:50 +0530

    Hello Toast added 03-06-2020

--- HelloToast/app/src/main/java/android/fdp/apssdc/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/android/fdp/apssdc/hellotoast/MainActivity.java
@@ -3,12 +3,35 @@ package android.fdp.apssdc.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView result;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // connecting count_tv with result
+        result = findViewById(R.id.count_tv);
+    }
+
+    /*The Following method will be invoked
+    as soon as the Toast button is pressed*/
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,"Hello Your Name",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    /*The Following method will be invoked
+    as soon as the Count button is pressed*/
+    public void showCount(View view) {
+        count++;
+        result.setText(String.valueOf(count));
     }
 }
